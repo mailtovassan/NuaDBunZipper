c1c14e78839234e6f2882287ab5d01589fda5feb
Vassan <mailtovassan@gmail.com>
2013-03-07 20:33:31 +0530

    NuaDBunZipper
    NuaDBunZipper

---
+++ NuaDBunZipper.h
@@ -0,0 +1,13 @@
+//
+//  NuaDBunZipper.h
+//  NuaDBunZipper
+//
+//  Created by Ingage on 07/03/13.
+//  Copyright (c) 2013 MyInGage. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NuaDBunZipper : NSObject
+-(id)initdb:(NSString*)dbname;
+@end
